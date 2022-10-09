5d3040af978793d673999353a17bc2cb133fc8a5
singharaj usai <djjd22938@gmail.com>
2022-10-09 12:13:51 -0400

    webpackpwamanifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,24 @@ module.exports = () => {
         template: './index.html',
         title: 'PWA Text Editor'
       }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
