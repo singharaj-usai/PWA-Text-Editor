cfdd29e7bad44e005abfbaba4149611ce313cacc
singharaj usai <djjd22938@gmail.com>
2022-10-09 13:05:49 -0400

    fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,6 +40,10 @@ module.exports = () => {
           },
         ],
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
