02d4cca810fff77cd06ddbb2eea84114bb219c76
singharaj usai <djjd22938@gmail.com>
2022-10-09 12:42:50 -0400

    click event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,12 +4,15 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 window.deferredPrompt = event;
-butInstall.classList.toggle('hidden', false); 
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
+// click EventListener
 butInstall.addEventListener('click', async () => {
-
+const promptEvent = window.deferredPrompt;
+if (!promptEvent) {return;}
+promptEvent.prompt();
+window.deferredPrompt = null;  
 });
 
 // TODO: Add an handler for the `appinstalled` event
